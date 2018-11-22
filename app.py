9c4bcf73fcfb927c28de72819daf8facc4dd64ae
aboud20-meet <aboud20@meet.mit.edu>
2018-11-22 18:06:49 +0200

    trying this out

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World test!'
+    return render_template('home.html') 
 
 if __name__ == '__main__':
     app.run(debug=True)
